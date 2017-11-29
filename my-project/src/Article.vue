11d94ae55dd3fc4348feeaad1bfa5b3dbd1b7401
SampurnaC <sampurna.chapagainnn@gmail.com>
2017-11-29 15:21:27 +0545

    add events

--- my-project/src/Article.vue
+++ my-project/src/Article.vue
@@ -2,8 +2,12 @@
   <div class="article">
     <h1>{{ title }}</h1>
     <p>published on {{ published | moment }}</p>
+    <p>Shares: {{ shares }} </p>
     <p class="lead">{{ content }}</p>
     <app-author :author = "author"></app-author>
+
+    <br><br>
+    <app-social-sharing @articleWasShared="shared('Hello', $event)"></app-social-sharing>
   </div>
 </template>
 
@@ -11,6 +15,7 @@
 
   import moment from 'moment';
   import Author from './Author.vue';
+  import Social from './Social.vue';
 
   export default {
     data() {
@@ -22,8 +27,15 @@
         author: {
           firstName: 'Sampurna',
           lastName: 'Chapagain'
-        }
+        },
+        shares: 0
       };
+    },
+    methods: {
+      shared: function(event) {
+        this.shares++;
+        console.log(event)
+      }
     },
     filters: {
       moment: function(value){
@@ -32,7 +44,8 @@
     },
 
     components: {
-      appAuthor: Author
+      appAuthor: Author,
+      appSocialSharing: Social
     }
   }
 </script>
